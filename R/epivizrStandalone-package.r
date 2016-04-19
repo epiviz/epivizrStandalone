55b53910bbd5e23f037a3d55cb21c62d1f90495d
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2016-04-19 17:32:41 -0400

    documentation, save user params to local folder (.txt file), start a standalone server

--- R/epivizrStandalone-package.r
+++ R/epivizrStandalone-package.r
@@ -1,29 +1,62 @@
-#' epivizrStandalone.
-#'
-#' @name epivizrStandalone
-#' @docType package
+#' Initialize Epiviz Standalone to either a local epiviz repository or clones from github.
 #' @import git2r
+#' @import epivizr
+#' @param url (character) github url to use. defaults to (\url{"https://github.com/epiviz/epiviz.git"}).
+#' @param branch (character) branch on the github repository. defaults to (master).
+#' @param local_path (character) if you already have a local instance of epiviz and would like to run standalone use this.
+#' 
+#' @return path to the standalone location
+#' 
+#' @examples
+#' see package vignete for example usage
+#' initStandalone(url="https://github.com/epiviz/epiviz.git", branch="master")
+#' 
 #' @export
 
-getStandaloneLocation <- function() {
-  
-  path <- system.file(package = "epivizrStandalone")
-  webpath <- paste(path, '/www')
-  
-  repo <- git2r::repository(webpath)
-  repo
-}
 
-#' @export
-epivizrStandalone <- function(url="https://github.com/epiviz/epiviz.git", branch="master") {
+initStandalone <- function(url="https://github.com/epiviz/epiviz.git", branch="master", local_path=NULL) {
   
   path <- system.file(package = "epivizrStandalone")
-  webpath <- paste(path, '/www')
+  webpath <- file.path(path, 'www')
   
   unlink(webpath, recursive = TRUE)
   
-  packageStartupMessage("Cloning epiviz from git ...")
-  git2r::clone(url, local_path=webpath)
-  git2r::checkout(repo, branch)
+  params <- list(url=url, branch=branch, local_path=local_path)
+  dput(params, file=file.path(normalizePath('~'), ".txt"))
   
+  if(!is.null(local_path)) {
+    print("linking epiviz to local path ...")
+    file.symlink(from=local_path, to=webpath)
+  }
+  else {
+    print("Cloning epiviz from git ...")
+    git2r::clone(url, local_path=webpath)
+    if(branch != "master") {
+      git2r::checkout(repo, branch)    
+    }
+  }
+  
+  webpath
+}
+
+#' Uses the local instance of epiviz to start a stanadalone epivizr session 
+#' 
+#'  
+#' @return An object of class \code{\link{EpivizApp}}
+#' 
+#' @examples
+#' see package vignete for example usage
+#' app <- startStandalone()
+#' app$stop_app()
+#' 
+#' @export
+
+
+startStandalone <- function() {
+  
+  path <- system.file(package = "epivizrStandalone")
+  webpath <- file.path(path, 'www')
+  
+  server <- epivizrServer::createServer(static_site_path = webpath)
+  app <- epivizr::startEpiviz(server=server, host="http://localhost", path="/index-standalone.html", http_port=server$.port)
 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,25 @@
 .onLoad <- function(libname = find.package("epivizrStandalone"), pkgname = "epivizrStandalone") {
   
   path <- system.file(package = "epivizrStandalone")
-  webpath <- paste(path, '/www')
+  webpath <- file.path(path, 'www')
   
-  packageStartupMessage("checking for repo updates")
-  
-  if(dir.exists(webpath)) {
-    packageStartupMessage("checking for epiviz updates ...")
-    repo <- git2r::repository(webpath)
-    git2r::pull(repo)
-  }
-  else {
-    packageStartupMessage("Cloning  epiviz from git ...")
-    git2r::clone("https://github.com/epiviz/epiviz.git", local_path=webpath)
+  if(file.exists(file.path(normalizePath('~'), ".txt"))) {
+    params <- dget(file=file.path(normalizePath('~'), ".txt")) 
+    
+    if(is.null(params$local_path)) {
+      if (!is.null(params$url) & !is.null(params$branch)){
+        if(dir.exists(webpath)) {
+          packageStartupMessage("checking for epiviz updates ...")
+          repo <- git2r::repository(webpath)
+          git2r::pull(repo)
+          packageStartupMessage("Done")
+        }
+        else {
+          packageStartupMessage("Cloning  epiviz from git ...")
+          git2r::clone("https://github.com/epiviz/epiviz.git", local_path=webpath)
+          packageStartupMessage("Done")
+        }  
+      }
+    }  
   }
 }
